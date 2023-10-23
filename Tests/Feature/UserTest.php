632f0f6365f8cdf92a3fec7a608b0317f568031e
Maab Javaid <maab.javaid@easybell.de>
2023-10-23 01:38:23 +0100

    add positive testswith mock data

--- Tests/Feature/UserTest.php
+++ Tests/Feature/UserTest.php
@@ -2,13 +2,48 @@
 
 namespace MaabJavaid\UserLib\Tests\Feature;
 
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Http;
 use MaabJavaid\UserLib\Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    public function testUserPaginationList(): void
+    public function testSuccessfulUserPaginationList(): void
     {
-        $response = $this->getJson(route('requer-user.show', $this->preparePaginationRequestPayload()))
-        ->assertOk();
+        Http::fake([
+            config('user-lib.services.url') . 'users/*' => Http::response($this->paginatedUserListPayload(), Response::HTTP_OK),
+        ]);
+
+        $this->getJson(route('requer-user.index', $this->preparePaginationRequestPayload()))
+            ->assertOk();
+    }
+
+    public function testSuccessfulFetchSingleUser(): void
+    {
+        $mockPayload = [];
+        $mockPayload['data'] = $this->singleUserPayload();
+        Http::fake([
+            config('user-lib.services.url') . 'users/*' => Http::response($mockPayload, Response::HTTP_OK),
+        ]);
+
+        $this->getJson(route('requer-user.show', ['requer_user' => 1]))
+            ->assertOk();
+    }
+
+    public function testSuccessfulCreatedAUser(): void
+    {
+        $mockPayload = [
+            'name'      => $this->faker->name,
+            'job'       => $this->faker->jobTitle,
+            'id'        => $this->faker->numberBetween(1, 20),
+            'createdAt' => today()
+        ];
+
+        Http::fake([
+            config('user-lib.services.url') . 'users/*' => Http::response($mockPayload, Response::HTTP_CREATED),
+        ]);
+
+        $this->postJson(route('requer-user.store', $this->prepareStoreRequestPayload()))
+            ->assertOk();
     }
 }
